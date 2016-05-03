1215b2077364a64be6c80054c77c5e24c86456e3
Wouter de Vos <wouter@codaisseur.com>
2016-05-03 10:37:20 +0200

    Use Mongoose for Mongo

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ const rest = require('feathers-rest');
 const socketio = require('feathers-socketio');
 // const errors = require('feathers-errors');
 const bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient;
-const service = require('feathers-mongodb');
+const mongoose = require('mongoose');
+const service = require('feathers-mongoose');
+
+// Require your models
+const Game = require('./models/game');
+
+// Tell mongoose to use native promises
+// See http://mongoosejs.com/docs/promises.html
+mongoose.Promise = global.Promise;
+
+const mongo_url = process.env.MONGODB_URI || 'mongodb://localhost:27017/rockpaperscissors';
+mongoose.connect(mongo_url);
 
 // Create a feathers instance.
 const app = feathers()
@@ -17,27 +27,20 @@ const app = feathers()
   // Turn on URL-encoded parser for REST services
   .use(bodyParser.urlencoded({extended: true}));
 
-const mongo_url = process.env.MONGODB_URI || 'mongodb://localhost:27017/rockpaperscissors';
+app.use('/games', service({
+  Model: Game,
+  paginate: {
+    default: 30,
+    max: 50
+  }
+}));
 
-  // Connect to your MongoDB instance(s)
-MongoClient.connect(mongo_url).then(function(db){
-  // Connect to the db, create and register a Feathers service.
-  app.use('/games', service({
-    Model: db.collection('games'),
-    paginate: {
-      default: 30,
-      max: 50
-    }
-  }));
-  // A basic error handler, just like Express
-  // app.use(errors.handler());
+// A basic error handler, just like Express
+// app.use(errors.handler());
 
-  // Start the server
-  var port = process.env.PORT || 3030;
-  var server = app.listen(port);
-  server.on('listening', function() {
-    console.log('Feathers Message MongoDB service running on ' + port);
-  });
-}).catch(function(error){
-  console.error(error);
+// Start the server
+var port = process.env.PORT || 3030;
+var server = app.listen(port);
+server.on('listening', function() {
+  console.log('Feathers Message MongoDB service running on ' + port);
 });
